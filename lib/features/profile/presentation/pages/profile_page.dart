d42009a61ecdc8485ae87899ceffb9622103fecf
DevNadimm <nadimchowdhury87@gmail.com>
2025-09-12 02:56:10 +0600

    updated

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:vaccine_home/features/profile/data/models/profile_menu_item.dart';
+import 'package:vaccine_home/features/profile/presentation/widgets/profile_header.dart';
+import 'package:vaccine_home/features/profile/presentation/widgets/profile_section.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -6,17 +10,100 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
+      appBar: AppBar(title: const Text('Profile')),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const ProfileHeader(),
+            const SizedBox(height: 32),
 
-            ],
-          ),
+            /// Account Section
+            ProfileSection(
+              title: "Account",
+              items: [
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedEdit03,
+                  title: "Edit Profile",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedLockPassword,
+                  title: "Change Password",
+                  onTap: () {},
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            /// Vaccination Section
+            ProfileSection(
+              title: "Vaccination",
+              items: [
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedHealth,
+                  title: "Vaccine Request History",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedCalendar02,
+                  title: "Vaccine Schedule",
+                  onTap: () {},
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            /// Engagement Section
+            ProfileSection(
+              title: "Engagement",
+              items: [
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedDiscount01,
+                  title: "Advertisement / Offers",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedComment01,
+                  title: "Feedback",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedHelpCircle,
+                  title: "FAQ",
+                  onTap: () {},
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            /// Settings Section
+            ProfileSection(
+              title: "Settings",
+              items: [
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedNotification03,
+                  title: "Notifications",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedSecurity,
+                  title: "Privacy Policy",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedFile01,
+                  title: "Terms & Conditions",
+                  onTap: () {},
+                ),
+                ProfileMenuItem(
+                  icon: HugeIcons.strokeRoundedLogout03,
+                  title: "Logout",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
