597cd79b6a5564492cc8e05f6889862da86e8064
DevNadimm <nadimchowdhury87@gmail.com>
2025-09-18 18:53:04 +0600

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vaccine_home/app.dart';
 import 'package:vaccine_home/core/services/service_locator.dart';
+import 'package:vaccine_home/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   return runApp(
     DevicePreview(
