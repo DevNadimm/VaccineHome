92c23dac156fd5c1cee52b2f036b65e0a0d21537
DevNadimm <nadimchowdhury87@gmail.com>
2025-09-24 01:39:54 +0600

    refactor: update "request" to "order" in vaccine module

--- lib/features/vaccine_card/presentation/pages/vaccine_card_request_page.dart
+++ lib/features/vaccine_card/presentation/pages/vaccine_card_request_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hugeicons/hugeicons.dart';
 import 'package:intl/intl.dart';
 import 'package:vaccine_home/core/constants/colors.dart';
 import 'package:vaccine_home/core/constants/messages.dart';
 import 'package:vaccine_home/core/utils/enums/message_type.dart';
 import 'package:vaccine_home/core/utils/helper_functions/show_custom_bottom_sheet.dart';
+import 'package:vaccine_home/core/utils/widgets/app_bar_back_btn.dart';
 import 'package:vaccine_home/core/utils/widgets/app_notifier.dart';
 import 'package:vaccine_home/core/utils/widgets/custom_text_field.dart';
 import 'package:vaccine_home/core/utils/widgets/loader.dart';
@@ -91,14 +91,7 @@ class _VaccineCardRequestPageState extends State<VaccineCardRequestPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vaccine Card Request'),
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: const Icon(
-            HugeIcons.strokeRoundedArrowLeft01,
-            size: 32,
-            color: AppColors.primaryFontColor,
-          ),
-        ),
+        leading: const AppBarBackBtn(),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
