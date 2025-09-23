f4e875a70cba85e5f974180056dbae89c581d140
DevNadimm <nadimchowdhury87@gmail.com>
2025-09-24 02:22:20 +0600

    add AppBar back button

--- lib/features/vaccine/presentation/pages/vaccine_order_page.dart
+++ lib/features/vaccine/presentation/pages/vaccine_order_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hugeicons/hugeicons.dart';
 import 'package:vaccine_home/core/constants/colors.dart';
 import 'package:vaccine_home/core/constants/messages.dart';
 import 'package:vaccine_home/core/utils/enums/message_type.dart';
+import 'package:vaccine_home/core/utils/widgets/app_bar_back_btn.dart';
 import 'package:vaccine_home/core/utils/widgets/app_notifier.dart';
 import 'package:vaccine_home/core/utils/widgets/custom_text_field.dart';
 import 'package:vaccine_home/core/utils/widgets/loader.dart';
@@ -67,14 +67,7 @@ class _VaccineOrderPageState extends State<VaccineOrderPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vaccine Order'),
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: const Icon(
-            HugeIcons.strokeRoundedArrowLeft01,
-            size: 32,
-            color: AppColors.primaryFontColor,
-          ),
-        ),
+        leading: const AppBarBackBtn(),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
