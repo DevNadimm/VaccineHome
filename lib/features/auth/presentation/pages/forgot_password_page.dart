f2354718bbdb3d3f43b7414dec12fb5769d8ca36
DevNadimm <nadimchowdhury87@gmail.com>
2025-10-16 20:00:15 +0600

    minor update

--- lib/features/auth/presentation/pages/forgot_password_page.dart
+++ lib/features/auth/presentation/pages/forgot_password_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vaccine_home/core/constants/colors.dart';
-import 'package:vaccine_home/core/constants/messages.dart';
 import 'package:vaccine_home/core/utils/enums/message_type.dart';
 import 'package:vaccine_home/core/utils/widgets/app_bar_back_btn.dart';
 import 'package:vaccine_home/core/utils/widgets/app_notifier.dart';
