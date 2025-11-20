67556900403c43b8db4db0ca31cbcc6806edcc29
DevNadimm <nadimchowdhury87@gmail.com>
2025-11-20 17:59:51 +0600

    feat: remove unused imports

--- lib/features/profile/presentation/pages/edit_profile_page.dart
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:vaccine_home/core/constants/colors.dart';
 import 'package:vaccine_home/core/constants/messages.dart';
 import 'package:vaccine_home/core/services/app_preferences.dart';
